7cc461fe7381ad8cab2ecae277406c4917d271d7
xronak009 <teamxzonee@gmail.com>
2024-07-21 11:33:54 +0530

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return 'Hello from Flask!'
-
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
 import telebot
 import requests
 from bs4 import BeautifulSoup
